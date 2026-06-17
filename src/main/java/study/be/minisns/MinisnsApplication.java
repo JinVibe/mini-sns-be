dae35dab3943e603f03dc2b85b354500575c04e6
JinVibe <loonaticvibe2.11@gmail.com>
2026-06-17 21:19:31 +0900

    Initial commit

---
+++ src/main/java/study/be/minisns/MinisnsApplication.java
@@ -0,0 +1,13 @@
+package study.be.minisns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinisnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinisnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/be/minisns/MinisnsApplicationTests.java
@@ -0,0 +1,13 @@
+package study.be.minisns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinisnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
